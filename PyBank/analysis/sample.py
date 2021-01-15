0f30c5b542af444d67c5298ff024ab5a3e80c7d4
tarapolli <tara_polli@yahoo.com>
2021-01-15 11:46:08 -0500

    latest changes to PyBank

--- PyBank/analysis/sample.py
+++
@@ -1,7 +0,0 @@
-count_rows = 0
-Profit_loss = 0
-<Write a for loop for every row>
-  Kep adding 1 to count_rows
-  profit_loss = profit_loss + row[1]
-  #Crate a list with differences between the profit/loss of subsequents months
-  #Get the difference between current row & previous row
