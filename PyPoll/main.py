f66e14c77e5c5a8d9721078565eab2192a469e83
tarapolli <tara_polli@yahoo.com>
2021-01-17 15:58:42 -0500

    final homework submission

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,41 +1,57 @@
+#import modules
 import os
 import csv
 
-election_csv = os.path.join('resources', 'election_data.csv')
+#set local pc path to data csv file
+csvpath = os.path.join("resources", "election_data.csv")
 
-# create lists for holding data
-total_votes = []
-candiate_name = []
-total_candidate = []
-
-# create variables
-total_votes = 0
+#create votes dictionary
+candidates_list = {}
+counter = 0
 
 # open and read csv
-with open(election_csv) as csv_file:
-    election_data = csv.reader(csv_file, delimiter=",")
+with open(csvpath) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
 
     # skip header row
-    next(election_data)
+    next(csv_reader, None)
 
-    # begin loop to calculate total votes included in dataset
-    for row in election_data:
-        total_votes
-        total_votes = total_votes + 1
- 
+    # begin loop
+    for row in csv_reader:
+        #define candidate column
+        candidate = row[2]
 
-        # list of candidates who received votes
-        for row in election_data:
-            total_votes = total_votes +1
+        #if candidate not in dictionary, add with first vote
+        if candidate not in candidates_list:
+            candidates_list[candidate] = 1
+
+        #if candidate already in dictionary, add a vote
+        else:
+            candidates_list[candidate] += 1
+
+#for key, value in votes.items():
+    #print(f'{key} : {value}')
+
+# add values in dictionary to get total votes
+votes = candidates_list.values()
+Total_Votes = sum(votes)
+
+# use max function to identify winning candidate
+# def get_max(Total_Votes):
+#     votemax = 0
+#     for votemax in candidate:
+#         if votemax < candidates_list
+#     return votemax
+#     maxcandidate = get_max(candidates)
+    #print(maxcandidate)
+
+# print summary table
+print(f'Election Results')
+print(f'Total Votes: {Total_Votes}')
+#for key, value in candidates_list.items():
+#    print(f'{candidate}: {votes / Total_Votes: 3%} ({Total_Votes})")
 
 
-        # percentage of votes each candidate won
-        
-
-        # total number of votes each candidate won
-            #count_votes = len(candiate_name)
-
-        
-
-
-# winner of the election based on popular vote
+# write summary table to output file
+# specify file to write to
+output_path = os.path.join("analysis", "PyPoll_Summary.txt")
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# python-challenge
+This Python Challenge encompasses two projects:
+
+PyBank
+This project runs a Python script for analyzing the financial records of a company Using a set of financial data called budget_data.csv. The dataset is composed of two columns: Date and Profit/Losses. The results identify the following:
+  - The total number of months included in the dataset
+  - The net total amount of "Profit/Losses" over the entire period
+  - Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+  - The greatest increase in profits (date and amount) over the entire period
+  - The greatest decrease in losses (date and amount) over the entire period
+
+PyPoll
+This project runs a Python script that analyzes the votes and calculates the results; see below. Ideally, this process will help this small, rural town modernize its vote counting process. Using dataset called election_data.csv, it contains three columns: Voter ID, County, and Candidate. 
+  - The total number of votes cast
+  - A complete list of candidates who received votes
+  - The percentage of votes each candidate won
+  - The total number of votes each candidate won
+  - The winner of the election based on popular vote.
+
+
+
+
+
+
+
