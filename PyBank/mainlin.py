fc0f58c71d0b3b6bc85d5d5f75d8b495638e2c27
tarapolli <tara_polli@yahoo.com>
2021-01-17 14:19:38 -0500

    final PyBank

--- PyBank/mainlin.py
+++ PyBank/mainlin.py
@@ -9,12 +9,13 @@ total_profit = []   # store all of profit/losses from column 2 []]
 month_diff = []     # difference b/t each month
 month__diff_add = []   # store all months in difference b/t current and previous rows
 
-# create counters
+
 count = 0
 max_increase_month = ''
 max_decrease_month = ''
 max_increase = 0
 max_decrease = 9999999
+
 
 # open and read csv
 with open(csvpath) as csv_file:
@@ -23,6 +24,7 @@ with open(csvpath) as csv_file:
     # skip header row
     next(csv_reader, None)
 
+
     # begin loop on first row
     for row in csv_reader:
 
@@ -30,7 +32,7 @@ with open(csvpath) as csv_file:
             # keep adding 1 to count rows  # profit_loss = profit_loss + row[1]
             count = count + 1
             # store this new value in list
-            new_profit_loss = int(row[1]) 
+            new_count = int(row[1])  ###
             # append variable counters
             total_months.append(str(row[0]))
             total_profit.append(int(row[1]))
@@ -39,10 +41,10 @@ with open(csvpath) as csv_file:
             total_months.append(str(row[0]))
             total_profit.append(int(row[1]))
             # calculate difference between current row & previous row
-            month_diff.append(int(int(row[1])  - new_profit_loss))
+            month_diff.append(int(int(row[1])  - new_count))
             # add to difference in months
             month__diff_add.append(str(row[0]))
-            diff = int(row[1])  - new_profit_loss
+            diff = int(row[1])  - new_count
             if diff > max_increase:
                 max_increase = diff
                 max_increase_month = row[0]
@@ -50,7 +52,7 @@ with open(csvpath) as csv_file:
                 max_decrease = diff
                 max_decrease_month = row[0]
             # revised list containing all month changes
-            new_profit_loss = int(row[1])
+            new_count = int(row[1])
 
     # calculate results for summary table
    
@@ -70,18 +72,58 @@ with open(csvpath) as csv_file:
         return average
     AverageChange = round(get_ave_change(month_diff),2)
 
-    
+    # maximum change
+    def get_max(list):
+        countmax = 0
+        for number in list:
+            if countmax < number:
+                countmax = number
+        return countmax
+    MaxChange = get_max(month_diff)
+
+    # minimum change
+    def get_min(list):
+        countmin = 0
+        for number in list:
+            if countmin > number:
+                countmin = number
+        return countmin
+    MinChange = get_min(month_diff)
 
 # print summary table here
-print(f"Financial Analysis")
-print(f"------------------")
-print(f'Total Months: {TotalMonths}')
-print(f'Total Profit: $ {TotalProfit}')
-print(f'Average Change: {AverageChange}')
-print(f'Greatest Increase In Profits: {max_increase_month} ($ {max_increase})')
-print(f'Greatest Decrease In Profits: {max_decrease_month} ($ {max_decrease})')
-
+print("Financial Analysis")
+print("------------------")
+print("Total Months: " + str(TotalMonths))
+print("Total Profit: $" + str(TotalProfit))
+print("Average Change: " + str(AverageChange))
+print("Greatest Increase In Profits:" +  " $" + str(MaxChange))
+print("Greatest Decrease In Profits:" +  " $" + str(MinChange))
 
 # write summary table to output file
 # specify file to write to
-output_path = os.path.join("analysis", "PyBank_Summary.txt")
+output_path = os.path.join("analysis", "PyBank_Summary.csv")
+
+# open file using "write" mode. 
+with open(output_path, 'w', newline='') as csvfile:
+    # initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=',')
+
+
+
+# maximum change
+    #def get_max(list):
+        #countmax = 0
+        #for number in list:
+            #if countmax < number:
+                #countmax = number
+        #return countmax
+    #MaxChange = get_max(month_diff)
+
+    # minimum change
+    #def get_min(list):
+        #countmin = 0
+        #for number in list:
+            #if countmin > number:
+                #countmin = number
+        #return countmin
+    #MinChange = get_min(month_diff)
